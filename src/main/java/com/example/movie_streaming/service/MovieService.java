b52f5c7c6f882e84d4bdd266caad9d309950a29a
Amisha Khunte <amishakhunte60@gmail.com>
2026-02-02 18:55:47 +0530

    Add Movie repository layer

---
+++ src/main/java/com/example/movie_streaming/service/MovieService.java
@@ -0,0 +1,11 @@
+package com.example.movie_streaming.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+
+    @Autowired
+
+}
