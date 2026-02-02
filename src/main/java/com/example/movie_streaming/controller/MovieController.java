a40b6b12c1edc695f4fbd6e0f5da92f7353f1934
Amisha Khunte <amishakhunte60@gmail.com>
2026-02-02 19:40:01 +0530

    Add movieController Layer

---
+++ src/main/java/com/example/movie_streaming/controller/MovieController.java
@@ -0,0 +1,31 @@
+package com.example.movie_streaming.controller;
+
+import com.example.movie_streaming.entity.Movie;
+import com.example.movie_streaming.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @PostMapping
+    public Movie addMovie(@RequestBody Movie movie){
+        return movieService.addMovie(movie);
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies(){
+        return movieService.getAllMovies();
+    }
+
+    @GetMapping("/{id}")
+    public Movie getMovieById(@PathVariable Long id){
+        return movieService.getMovieByID(id);
+    }
+}
