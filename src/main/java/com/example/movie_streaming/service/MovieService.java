ad627af4206eeff94859624f3b9facd302ccb795
Amisha Khunte <amishakhunte60@gmail.com>
2026-02-02 19:26:38 +0530

    Add MovieService Layer

--- src/main/java/com/example/movie_streaming/service/MovieService.java
+++ src/main/java/com/example/movie_streaming/service/MovieService.java
@@ -1,11 +1,35 @@
 package com.example.movie_streaming.service;
 
+import com.example.movie_streaming.entity.Movie;
+import com.example.movie_streaming.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
 
     @Autowired
+    private MovieRepository movieRepository;
+
+    public Movie addMovie(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Movie getMovieByID(long id) {
+        Optional<Movie> movie = movieRepository.findById(id);
+        return movie.orElse(null);
+    }
 
 }
+
+
+
+
+
